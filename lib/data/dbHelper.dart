69252886890f46a22b2254da787ef2a782850980
İsmail <olgunerismail2@gmail.com>
2023-07-24 10:41:47 +0300

    Asenkron Yapısı

--- lib/data/dbHelper.dart
+++ lib/data/dbHelper.dart
@@ -1,3 +1,25 @@
-class DbHelper{
+import 'dart:async';
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+abstract class DbHelper{
+
+  late Database _db;
+  Future<Database> get db async {
+    if (_db==null){
+      _db= await intializeDb();
+  }
+    return _db;
+}
+
+  Future<Database> intializeDb() async{
+    String dbPath = join (await getDatabasesPath(),"etrade.db");
+    var eTradeDb= await openDatabase(dbPath,version: 1,onCreate: createDb);
+    return eTradeDb;
+  }
+
+  void createDb(Database db, int version) async {
+    await db.execute("Create Table products(id integer primary key,name text, description text,unitPrice integer)");
+  }
 }
