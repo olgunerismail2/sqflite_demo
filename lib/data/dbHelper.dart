7402cbe69a8e0ee3479ad7ca00965a76082671a2
İsmail <olgunerismail2@gmail.com>
2023-07-25 11:27:25 +0300

    Crud operasyonları

--- lib/data/dbHelper.dart
+++ lib/data/dbHelper.dart
@@ -2,24 +2,52 @@ import 'dart:async';
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_demo/models/product.dart';
 
-abstract class DbHelper{
-
+abstract class DbHelper {
   late Database _db;
   Future<Database> get db async {
-    if (_db==null){
-      _db= await intializeDb();
-  }
+    if (_db == null) {
+      _db = await intializeDb();
+    }
     return _db;
-}
+  }
 
-  Future<Database> intializeDb() async{
-    String dbPath = join (await getDatabasesPath(),"etrade.db");
-    var eTradeDb= await openDatabase(dbPath,version: 1,onCreate: createDb);
+  Future<Database> intializeDb() async {
+    String dbPath = join(await getDatabasesPath(), "etrade.db");
+    var eTradeDb = await openDatabase(dbPath, version: 1, onCreate: createDb);
     return eTradeDb;
   }
 
   void createDb(Database db, int version) async {
-    await db.execute("Create Table products(id integer primary key,name text, description text,unitPrice integer)");
+    await db.execute(
+        "Create Table products(id integer primary key,name text, description text,unitPrice integer)");
+  }
+
+  Future<List<Product>> getProducts() async {
+    Database db = await this.db;
+    var result = await db.query("products");
+    return  List.generate(result.length,(i){
+      return Product.fromObject(result[i]);
+    });
+  }
+
+  Future<int> insert(Product product) async {
+    Database db = await this.db;
+
+    var result = await db.insert("products",product.toMap());
+    return result;
+  }
+  Future<int> delete(int id) async {
+    Database db = await this.db;
+
+    var result = await db.rawDelete("delete from where id= $id");
+    return result;
+  }
+  Future<int> update (Product  product) async {
+    Database db = await this.db;
+
+    var result = await db.update("products", product.toMap(),where: "id=?",whereArgs: [product.id]);
+    return result;
   }
 }
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,9 +1,29 @@
-class Product{
+class Product {
   late int id;
   late String name;
- late String description;
- late double unitPrice;
+  late String description;
+  late double unitPrice;
 
   Product(this.id, this.name, this.description, this.unitPrice);
   Product.withId(this.id, this.name, this.description, this.unitPrice);
+ Map<String,dynamic>toMap()   {
+
+var map= Map<String,dynamic>();
+   map["name"]= name;
+   map["description"]= description;
+   map["unitPrice"]=unitPrice;
+
+   if(id!=null){
+     map["id"]=id;
+  }
+  return map;
+
 }
+ Product.fromObject(dynamic o){
+   this.id=o["id"];
+   this.name= o["name"];
+   this.description= o["description"];
+   this.unitPrice= o["unitPrice"];
+ }
+  }
+
